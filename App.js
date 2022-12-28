927afe4d3c8989a273f006b4e8e6c3faa665447a
Sarah-Lisa Mero <sarahlisamero@gmail.com>
2022-12-28 23:11:25 +0100

    app toegevoegd met navigatie en import screens

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import products from './screens/ProductScreen';
+import details from './screens/DetailsScreen';
+import cart from './screens/CartScreen';
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="products" component={products} />
+        <Stack.Screen name="details" component={details} />
+        <Stack.Screen name="cart" component={cart} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
