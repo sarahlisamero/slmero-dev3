d8657b9a69f6bfcd834063e6bc7131998796cff3
Sarah-Lisa Mero <sarahlisamero@gmail.com>
2022-12-30 22:35:49 +0100

    route toegevoegd

--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -3,21 +3,10 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import React, {useState, useEffect} from 'react';
 import {Stylesheet, Text, View, Image, TextInput, Pressable, FlatList} from 'react-native';
-
-const Stack = createNativeStackNavigator();
-
 import ProductDetails from '../components/ProductDetails';
 
-const details = ({route}) => {
-    const {productId} = route.params;
-
-    return (
-        <View>
-          <ProductDetails productId={productId} />
-        </View>
-      );
-}
-/*const details = ({navigation, route}) =>{
+const Stack = createNativeStackNavigator();
+/*const Details = ({navigation, route}) =>{
 
     return (
         <View>
@@ -27,16 +16,23 @@ const details = ({route}) => {
            </Text>
         </View>
     )
-}*/
-/*const details = ({navigation, route}) =>{
+}
+export default Details;*/
+
+const Details = ({navigation, route}) =>{
 
     return (
         <View>
+            <ProductDetails
+            title={route.params.itemTitle}
+            />
            <Text>
-            {route.params.itemTitle}
-            
+           {route.params.itemDesc} 
            </Text>
         </View>
     )
-}*/
-export default details;
+}
+console.log(Details)
+export default Details;
+
+
