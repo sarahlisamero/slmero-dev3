45a52fc93b69b05ae43de625df4b0034144f441b
Sarah-Lisa Mero <sarahlisamero@gmail.com>
2022-12-30 22:40:01 +0100

    component van details gemaakt, ook hier werkt afbeelding niet

--- components/ProductDetails.js
+++ components/ProductDetails.js
@@ -1,50 +1,39 @@
 
-import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
+/*import { useNavigation } from '@react-navigation/native'; //nieuw
+
+import React, {useState, useEffect} from 'react';
+import { View, Text, StyleSheet, TouchableNativeFeedback, Pressable, Image, Button } from 'react-native';
+import { initialWindowMetrics } from 'react-native-safe-area-context';
 
 
-const ProductDetails = props => { /*ProductDetails*/
-  const [productDetails, setPoductDetails] = useState({});
-
-  const getProductDetailsById = async () => {
-    try {
-      const url = encodeURI("https://sarahlisamero.be/index.php/wp-json/wp/v2/posts/" + props.productId + "/")
-      const response = await fetch (url, { 
-
-      })
-      const json = await response.json();
-      setPoductDetails(json.results);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  useEffect(() => {
-    getProductDetailsById();
-  }, []);
-
+const ProductDetails = props => { 
+const navigation = useNavigation(); //nieuw
   return (
-    <ScrollView>
-      <Text style={styles.title}>{productDetails.title}</Text>
-      <View style={styles.details}>
-        <Text>{productDetails.description}</Text>
+      <View>
+        <Text>{props.title}</Text>
+        <Text>{props.description}</Text>
       </View>
+  );
+}
+export default ProductDetails;*/
 
-    </ScrollView>
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useNavigation } from '@react-navigation/native'; //nieuw
+
+import React, {useState, useEffect} from 'react';
+import { View, Text, StyleSheet, TouchableNativeFeedback, Pressable, Image, Button } from 'react-native';
+
+
+const ProductDetails = props => { /*ProductItem*/
+  return (
+      <View>
+        <Text>{props.title}</Text>
+        <Image source={require('../assets/sport-shoe.png')}></Image>
+        <Text>{props.description}</Text>
+        <Image source={{uri: `${props.image}`}}/> 
+      </View>
   );
 }
 
-const styles = StyleSheet.create({
-  title: {
-    margin: 16,
-    fontSize: 24,
-    textAlign: 'center',
-  },
-  details: {
-    borderWidth: 1,
-    padding: 16,
-    margin: 8,
-  },
-  
-});
 export default ProductDetails;
