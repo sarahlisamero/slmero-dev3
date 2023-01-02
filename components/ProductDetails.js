e5ffbdf188f653ba645808ac1df4f4fbe3d56215
Sarah-Lisa Mero <sarahlisamero@gmail.com>
2023-01-02 19:49:54 +0100

    uiteindelijk hetzelfde

--- components/ProductDetails.js
+++ components/ProductDetails.js
@@ -1,29 +1,4 @@
-
-/*import { useNavigation } from '@react-navigation/native'; //nieuw
-
-import React, {useState, useEffect} from 'react';
-import { View, Text, StyleSheet, TouchableNativeFeedback, Pressable, Image, Button } from 'react-native';
-import { initialWindowMetrics } from 'react-native-safe-area-context';
-
-
-const ProductDetails = props => { 
-const navigation = useNavigation(); //nieuw
-  return (
-      <View>
-        <Text>{props.title}</Text>
-        <Text>{props.description}</Text>
-      </View>
-  );
-}
-export default ProductDetails;*/
-
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { useNavigation } from '@react-navigation/native'; //nieuw
-
-import React, {useState, useEffect} from 'react';
-import { View, Text, StyleSheet, TouchableNativeFeedback, Pressable, Image, Button } from 'react-native';
-
+import { View, Text, StyleSheet, Image} from 'react-native';
 
 const ProductDetails = props => { /*ProductItem*/
   return (
