64f0f18c201834fbc82b54df80350ad20da05403
Sarah-Lisa Mero <sarahlisamero@gmail.com>
2023-01-06 22:54:16 +0100

    reviews scherm toegevoegd aan appjs

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import Products from './screens/ProductScreen';
 import Details from './screens/DetailsScreen';
 import Cart from './screens/CartScreen';
 import Terms from './screens/TermsScreen';
+import Reviews from './screens/ReviewsScreen';
 
 
 const Stack = createNativeStackNavigator();
@@ -17,6 +18,7 @@ export default function App() {
         <Stack.Screen name="Details" component={Details} />
         <Stack.Screen name="Cart" component={Cart} />
         <Stack.Screen name="Terms" component={Terms} />
+        <Stack.Screen name="Reviews" component={Reviews}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
