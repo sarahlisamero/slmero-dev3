2c74b6f5f42a727384248a1090029e5ca6c7c74b
Sarah-Lisa Mero <sarahlisamero@gmail.com>
2023-01-06 22:56:15 +0100

    component voor review gemaakt

---
+++ components/Review.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+ 
+const Review = props => {
+  return (
+    <View style={styles.container}> 
+        <Text style={styles.title}>Anoniempje: </Text>
+        <Text>{props.reviewInput}</Text>
+    </View>
+  );
+}
+ 
+export default Review;
+
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor: '#add8e6',
+        margin: 20,
+        padding: 10,
+        borderRadius:10
+    },
+    title:{
+        fontWeight:"bold",
+        paddingBottom:10
+    }
+});
