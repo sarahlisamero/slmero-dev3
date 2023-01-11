234952432d435c61424352b4a3e94b8478a92a43
Sarah-Lisa Mero <sarahlisamero@gmail.com>
2023-01-11 22:33:25 +0100

    opkuis

--- screens/TermsScreen.js
+++ screens/TermsScreen.js
@@ -1,10 +1,4 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import React, {useState, useEffect} from 'react';
-import {StyleSheet, Text, View, ScrollView, TouchableHighlight, TouchableOpacity} from 'react-native';
-
-const Stack = createNativeStackNavigator();
+import {StyleSheet, Text, View, ScrollView, TouchableOpacity} from 'react-native';
 
 const Terms = () =>{
 
@@ -14,14 +8,14 @@ const Terms = () =>{
                 <Text style={styles.title}>
                 Terms & Conditions 
                 </Text>
-                <View> 
+                <View style={styles.subText}> 
                     <Text style={styles.subTitle}>General</Text>
                     <Text>
                     You agree that we may, in our sole discretion, terminate or suspend your access to all or part of the site with or without notice and for any reason, including, without limitation, breach of these Terms of Use. Any suspected fraudulent, abusive or illegal activity may be grounds for terminating your relationship and may be referred to appropriate law enforcement authorities.
                     Upon termination or suspension, regardless of the reasons therefore, your right to use the services available on this site immediately ceases, and you acknowledge and agree that we may immediately deactivate or delete your account and all related information and files in your account and/or bar any further access to such files or this site. We shall not be liable to you or any third party for any claims or damages arising out of any termination or suspension or any other actions taken by us in connection with such termination or suspension.
                     </Text>
                 </View>
-                <View>
+                <View style={styles.subText}>
                     <Text style={styles.subTitle}>Contribution License</Text>
                     <Text> 
                     You agree that we may, in our sole discretion, terminate or suspend your access to all or part of the site with or without notice and for any reason, including, without limitation, breach of these Terms of Use. Any suspected fraudulent, abusive or illegal activity may be grounds for terminating your relationship and may be referred to appropriate law enforcement authorities.
@@ -55,7 +49,7 @@ const styles = StyleSheet.create({
         marginBottom:10
     },
     container:{
-        margin:15
+        margin:15,
     },
     tile:{
         borderRadius: 20,
@@ -79,6 +73,11 @@ const styles = StyleSheet.create({
     subTitle:{
         fontWeight:'bold', 
         fontSize:20,
-        marginTop:10
+    },
+    subText:{
+        backgroundColor:'#add8e6',
+        marginVertical:15,
+        padding:15,
+        borderRadius:10
     }
 })
